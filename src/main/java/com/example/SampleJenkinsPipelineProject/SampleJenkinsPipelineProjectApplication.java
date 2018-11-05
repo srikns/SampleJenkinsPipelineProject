c558d7b6389aeb43bc42b804c830ebdd7b3dad7b
Srikant <srikns@yahoo.com>
2018-11-05 11:03:10 -0500

    first time

---
+++ src/main/java/com/example/SampleJenkinsPipelineProject/SampleJenkinsPipelineProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.SampleJenkinsPipelineProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJenkinsPipelineProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleJenkinsPipelineProjectApplication.class, args);
+	}
+}
